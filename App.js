23e0fe4970c1c91e0659a6335f84912a886c8887
rohansethi <rohansethi1999430@gmail.com>
2024-03-08 21:06:42 -0500

    added HomeScreen

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
- import { Text, View } from 'react-native';
+ import { SafeAreaView, Text, View } from 'react-native';
+ import HomeScreen from './screens/HomeScreen'
+
+
+ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-  <View className="flex-1 items-center justify-center bg-white" >
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-  </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
